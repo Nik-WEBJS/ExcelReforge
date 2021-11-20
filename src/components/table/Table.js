f021deecb7f94f5c009131d7d5ad83419819613e
Nik Fokin <nikita.fokin0123@gmail.com>
2021-11-20 22:31:01 +0600

    finish base framework functional

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,38 +1,10 @@
 import { ExcelComponent } from "@core/ExcelComponent";
+import { createTable } from "@/components/table/table.template";
 
 export class Table extends ExcelComponent{
     static className = 'excel__table'
 
     toHTML(){
-        return `    <div class="row">
-        <div class="row-info"></div>
-        <div class="row-data">
-            <div class="colomn">A</div>
-            <div class="colomn">B</div>
-            <div class="colomn">C</div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="row-info">
-            1
-        </div>
-        <div class="row-data">
-            <div class="cell selected">A1</div>
-            <div class="cell">B1</div>
-            <div class="cell">C1</div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="row-info">
-            2
-        </div>
-        <div class="row-data">
-            <div class="cell contenteditable">A1</div>
-            <div class="cell contenteditable">B1</div>
-            <div class="cell contenteditable">C1</div>
-        </div>
-    </div>`
+        return createTable(20)
     }
 }
